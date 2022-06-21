ede921e94421e994b043ae5b32169f8f3f94ff8f
jaldabaoth-code <grialazurabi@gmail.com>
2022-06-21 07:37:12 +0200

    reupload of Artisans-Consult-Avant-Stage

---
+++ demande-renseignements-estimation-travaux-loiret-45.php
@@ -0,0 +1,307 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Demande de devis : travaux de r&eacute;novation, estimation travaux, devis artisan, entreprise de r&eacute;novation</title>
+<meta name="Description" content="Demand de renseignement, demande de devis pour des travaux de r&eacute;novation ou de construction d'une maison ou d'un appartement,
+estimation de vos travaux par un courtier en travaux" />
+<meta name="Keywords" content="batiment orleans, batiment loiret, batiment 45, construction maison orleans, construction maison loiret, construction maison 45, courtage orleans, courtage loiret, courtage 45,
+renover une maison orleans, renover une maison loiret, renover une maison 45, travaux de maison orleans, travaux de maison loiret, travaux de maison 45, travaux de r&eacute;novation orleans, travaux de r&eacute;novation loiret,
+travaux de r&eacute;novation 45, traveaux orleans, traveaux loiret, traveaux 45, travaux b&acirc;timent orleans, travaux b&acirc;timent loiret, travaux b&acirc;timent 45, r&eacute;novation appartement orleans, r&eacute;novation appartement loiret,
+r&eacute;novation appartement 45, entreprise renovation orleans,entreprise renovation loiret, entreprise renovation 45, demande de devis orleans, demande de devis loiret, demande de devis 45, courtier travaux orleans,
+courtier travaux loiret,courtier travaux 45, courtage en travaux orleans, courtage en travaux loiret, courtage en travaux 45"/>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="shortcut icon" href="images/iconAC.ico" /><link href="css/styleAC.css" rel="stylesheet" type="text/css" />
+<script>function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+//-->
+</script>
+<script type="text/javascript" src="jQuery.easySlides/js/jquery-1.6.1.min.js"></script>
+<script type="text/javascript" src="jQuery.easySlides/js/jquery.easyslides.min.v1.1.js"></script>
+<link rel="stylesheet" type="text/css" href="jQuery.easySlides/css/easySlides.default.min.css" />
+<script type="text/javascript" src="slideshow/example_1.js"></script>
+<script>
+function valid_form() {
+	var couleur_obligatoire="red";
+	var couleur_normal="black";
+
+	var is_ok=1;
+	if (document.form_contact.nom.value=="") {is_ok=0; document.getElementById("formu_nom").style.color=couleur_obligatoire;} else document.getElementById("formu_nom").style.color=couleur_normal;	
+	if (document.form_contact.prenom.value=="") {is_ok=0; document.getElementById("formu_prenom").style.color=couleur_obligatoire;} else document.getElementById("formu_prenom").style.color=couleur_normal;	
+	if (document.form_contact.adresse.value=="") {is_ok=0; document.getElementById("formu_adresse").style.color=couleur_obligatoire;} else document.getElementById("formu_adresse").style.color=couleur_normal;	
+	if (document.form_contact.codepostal.value=="") {is_ok=0; document.getElementById("formu_codepostal").style.color=couleur_obligatoire;} else document.getElementById("formu_codepostal").style.color=couleur_normal;	
+	if (document.form_contact.ville.value=="") {is_ok=0; document.getElementById("formu_ville").style.color=couleur_obligatoire;} else document.getElementById("formu_ville").style.color=couleur_normal;	
+	if (document.form_contact.telephone.value=="") {is_ok=0; document.getElementById("formu_telephone").style.color=couleur_obligatoire;} else document.getElementById("formu_telephone").style.color=couleur_normal;	
+	if (document.form_contact.email.value=="") {is_ok=0; document.getElementById("formu_email").style.color=couleur_obligatoire;} else document.getElementById("formu_email").style.color=couleur_normal;	
+	if(is_ok==1) {document.form_contact.submit();}
+	else {alert("Merci de remplir tous les champs obligatoires.");}
+}
+
+</script>
+<link rel="stylesheet" type="text/css" href="slideshow/styles.css" />
+</head>
+<body onload="MM_preloadImages(images/menurealisationsover.gif','images/menucharteover.gif','images/menuinfosover.gif','images/menucontactover.gif','images/menuprestationsover.gif','images/menuaccueilover.gif')">
+
+<table width="1006" border="0" align="center" cellpadding="0" cellspacing="5">
+  <tr>
+    <td width="170" height="88"><a href="index.html"><img src="images/artisans-consult-courtier-travaux-loiret.gif" alt="Artisans Consult : demande renseignement" width="170" height="80" border="0" /></a></td>
+    <td height="88" colspan="2" align="left" valign="bottom"><img src="images/motifXSAC.gif" width="18" height="18" /></td>
+    <td width="86" height="88">&nbsp;</td>
+  </tr>
+  <tr>
+    <td width="170" height="25" bgcolor="#626157">&nbsp;</td>
+    <td height="25" colspan="2" nowrap="nowrap" bgcolor="#626157">
+	<div width="725" align="left"><a href="http://www.localhost:8000/"><img src="images/menuaccueil.gif" alt="accueil" name="Image1" width="99" height="27" border="0" id="Image1" onmouseover="MM_swapImage('Image1','','images/menuaccueilover.gif',1)" onmouseout="MM_swapImgRestore()" /></a><a href="http://www.localhost:8000/prestations/courtier-travaux-estimation-travaux-batiment-renovation.html"><img src="images/menuprestations.gif" alt="prestations" name="Image2" width="112" height="27" border="0" id="Image2" onmouseover="MM_swapImage('Image2','','images/menuprestationsover.gif',1)" onmouseout="MM_swapImgRestore()" /></a><a href="http://www.localhost:8000/realisations/travaux-renovation-construction-maison-travaux-peinture-loiret-45.html"><img src="images/menurealisations.gif" alt="realisations" name="Image3" width="120" height="27" border="0" id="Image3" onmouseover="MM_swapImage('Image3','','images/menurealisationsover.gif',1)" onmouseout="MM_swapImgRestore()" /></a><a href="http://www.localhost:8000/charte-qualite/artisans-qualifies-devis-renovation-travaux-renovation.html"><img src="images/menucharte.gif" alt="charte qualite" width="135" height="27" border="0" name="Image4"  id="Image4" onmouseover="MM_swapImage('Image4','','images/menucharteover.gif',1)" onmouseout="MM_swapImgRestore()" /></a><a href="http://www.localhost:8000/partenaires/partenaires-informations-artisans-consult-courtier-travaux.html"><img src="images/menuinfos.gif" alt="partenaires et infos" name="Image5" width="166" height="27" border="0" id="Image5" onmouseover="MM_swapImage('Image5','','images/menuinfosover.gif',1)" onmouseout="MM_swapImgRestore()" /></a><a href="http://www.localhost:8000/demande-renseignements-estimation-travaux-loiret-45.php"><img src="images/menucontactover.gif" alt="contact"  width="93" height="27" border="0"  /></a></div></td>
+    <td height="25" bgcolor="#A0A098">&nbsp;</td>
+  </tr>
+  <tr>
+    <td width="170">&nbsp;</td>
+    <td height="270" colspan="2" align="left" valign="middle"><img src="images/slider-environnement.jpg" alt="prestaions Artisans Consult" width="740" height="270" border="0" /></td>
+    <td align="left" valign="top"><img src="images/motifAC.gif" width="34" height="34" /></td>
+  </tr>
+  <tr>
+    <td width="170">&nbsp;</td>
+    <td height="70" colspan="2" class="texthead" align="left">Contactez-nous et définissons ensemble votre projet !</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+  <td width="170">&nbsp;</td>
+    <td height="400" width="200" valign="top"><div class="textintro">
+      <p>Contactez-nous directement par téléphone ou laissez-nous vos coordonnées et votre message afin que nous puissions vous rappeler rapidement.</p>
+      <p>&nbsp;</p>
+      <p><img src="images/artisans-consult-courtier-travaux-loiret.gif" alt="Artisans Consult" width="170" height="80" /></p>
+      <p><strong>Patrice CHAUVIN</strong></p>
+      <p>47, rue des Plantes
+      <br>45750 St PRYVE St MESMIN</p>
+      <p>Tél. : 09 67 00 85 03
+      <br>Port. : 06 70 79 54 69
+      <br>Mail : <a href="mailto:contact@artisansconsult.fr">contact@artisansconsult.fr</a></p>
+    </div>
+    </td>
+<td width="500" align="left" valign="top" class="text">
+<?php 
+// Cas 1 : le formualire a été validé et on traite les données
+if(isset($_REQUEST["formulaire_valide"]) && $_REQUEST["formulaire_valide"]==1)
+{
+
+	// 1. Récupération des champs du POST de l'utilisateur
+	$nom ="";
+	$prenom ="";
+	$societe ="";
+	$adresse ="";
+	$codepostal ="";
+	$ville ="";
+	$telephone ="";
+	$email ="";
+	$type_maison ="";
+	$type_maison_precision ="";
+	$age_maison ="";
+	$annee_construct ="";
+	$texte ="";
+	$source ="";
+	$autre_source ="";
+
+
+	if(isset($_REQUEST["nom"])) $nom =$_REQUEST["nom"];
+	if(isset($_REQUEST["prenom"])) $prenom =$_REQUEST["prenom"]; 
+	if(isset($_REQUEST["societe"])) $societe =$_REQUEST["societe"];
+	if(isset($_REQUEST["adresse"])) $adresse =$_REQUEST["adresse"];
+	if(isset($_REQUEST["codepostal"])) $codepostal =$_REQUEST["codepostal"];
+	if(isset($_REQUEST["ville"])) $ville =$_REQUEST["ville"];
+	if(isset($_REQUEST["telephone"])) $telephone =$_REQUEST["telephone"];
+	if(isset($_REQUEST["email"])) $email =$_REQUEST["email"];
+	if(isset($_REQUEST["type_maison"])) $type_maison =$_REQUEST["type_maison"];
+	if(isset($_REQUEST["preciser_votre_type_de_bien"])) $type_maison_precision =$_REQUEST["preciser_votre_type_de_bien"];
+	if(isset($_REQUEST["age_maison"])) $age_maison =$_REQUEST["age_maison"];
+	if(isset($_REQUEST["preciser_annee_construction"])) $annee_construct =$_REQUEST["preciser_annee_construction"];
+	if(isset($_REQUEST["texte"])) $texte =$_REQUEST["texte"];
+	if(isset($_REQUEST["source"])) $source =$_REQUEST["source"];
+	if(isset($_REQUEST["autre_source"])) $autre_source =$_REQUEST["autre_source"];
+
+	// 2. Envoyer un mail
+	$replyemail="contact@artisansconsult.fr";
+	$thesubject=utf8_decode("[ARTISANS CONSULT] Coordonnées Contact");
+	$themessage=utf8_decode(stripslashes("Un nouveau contact vient de saisir ses coordonnées sur le site ARTISANS CONSULT :
+
+	- Nom : ".strtoupper($nom)."
+	- Prenom : ".strtoupper($prenom)."
+	- Société : ".strtoupper($societe)."
+	- Adresse : ".$adresse." - ".$codepostal." ".$ville."
+	- Telephone : ".$telephone."
+	- Email : ".$email."
+	- Type maison : ".$type_maison." ".$type_maison_precision."
+	- Age maison : ".$age_maison." ".$annee_construct."
+	- Source connaissance du site : ".$source." ".$autre_source."
+	- Projet : ".$texte."
+
+	Merci de le recontacter dans les plus proches délais."));
+
+	$email="contact@artisansconsult.fr";
+
+	mail($replyemail,
+		 $thesubject,
+		 $themessage,
+		 "From: ".$email."\nReply-To: ".$email);
+
+	// 3. Répondre à l'utilisateur que sa demande a bien été enregistrée
+	echo "<center>".strtoupper($_REQUEST["prenom"])." ".strtoupper($_REQUEST["nom"])."<br/>Votre demande a bien &eacute;t&eacute; transmise. Nous vous contacterons dans les plus proches d&eacute;lais.</center>";
+
+}
+else // cas 2 : on demande la page pour la première fois ou bien on n'a pas validé le formulaire
+{
+
+?>  
+   <form method="POST"  name="form_contact" action="#">
+   <input name='formulaire_valide' type='hidden' value='1' />
+<table>
+<tr>
+  <th colspan="2" class="textbook">Vos coordonnées <span class="infolegal">( * : champs obligatoires )</span></th>
+</tr>
+<tr><td class="textcharte"><span id="formu_nom">Nom </span><span style="color: #F19300;"><strong>*</strong></span> : </td>
+  <td class="textcharte">
+    <input name='nom' type='text' size='50' />
+  </td>
+</tr>
+<tr>
+  <td class="textcharte"><span id="formu_prenom">Prénom  </span><span style="color: #F19300;"><strong>*</strong></span> : </td>
+  <td class="textcharte">
+    <input name='prenom' type='text' size='50' />
+  </td>
+</tr>
+<tr><td class="textcharte">Société : </td>
+  <td class="textcharte">
+    <input name='societe' type='text' size='50' />
+  </td>
+</tr>
+<tr><td class="textcharte"><span id="formu_adresse">Adresse  </span><span style="color: #F19300;"><strong>*</strong></span> : </td>
+  <td class="textcharte">
+    <input name='adresse' type='text' size='50' />
+  </td>
+</tr>
+<tr><td class="textcharte"><span id="formu_codepostal">Code postal  </span><span style="color: #F19300;"><strong>*</strong></span> : </td>
+  <td class="textcharte">
+    <input name='codepostal' type='text' size='50' />
+  </td>
+</tr>
+<tr><td class="textcharte"><span id="formu_ville">Ville  </span><span style="color: #F19300;"><strong>*</strong></span> : </td>
+  <td class="textcharte">
+    <input name='ville' type='text' size='50' />
+  </td>
+</tr>
+<tr><td class="textcharte"><span id="formu_telephone">Téléphone  </span><span style="color: #F19300;"><strong>*</strong></span> : </td>
+  <td class="textcharte">
+    <input name='telephone' type='text' size='50' />
+  </td>
+</tr>
+<tr><td class="textcharte"><span id="formu_email">Email  </span><span style="color: #F19300;"><strong>*</strong></span> : </td>
+  <td class="textcharte">
+    <input name='email' type='text' size='50' />
+  </td>
+</tr>
+<tr>
+  <th colspan="2" class="textbook"><p>&nbsp;<br />
+  <span style="color:#F19300;font-style:italic">Votre bien</span></p></th>
+</tr>
+<tr class="textcharte">
+  <td>Le type</td><td>
+<input name='type_maison' type='radio' value='maison' />Maison<br />
+<input name='type_maison' type='radio' value='appartement' />Appartement<br />
+<input name='type_maison' type='radio' value='localcommercial' />Local commercial<br />
+<input name='type_maison' type='radio' value='autres' />Autres : <input type="text" name="preciser votre type de bien" id="preciser votre type de bien" /></td></tr>
+<tr class="textcharte">
+  <td>L'âge</td><td>
+<input name='age_maison' type='radio' value='neuf' />Neuf<br />
+<input name='age_maison' type='radio' value='ancien' />Ancien<br />
+<input name='age_maison' type='radio' value='annee_construction' />Année de construction : <input type="text" name="preciser_annee_construction" /></td></tr>
+<tr class="textcharte"><td>Vous êtes</td><td>
+<input name='proprio_maison' type='radio' value='proprietaire' />Propriétaire<br />
+<input name='proprio_maison' type='radio' value='locataire' />Locataire</td></tr>
+<tr>
+  <th colspan="2" class="textbook"><p>&nbsp;<br />
+  <span style="color:#F19300;font-style:italic">Votre projet de travaux</span></p></th>
+</tr>
+<tr><td>Votre message</td><td><textarea name='texte' cols='50' rows='20'></textarea></td></tr>
+<tr>
+  <th colspan="2" class="textbook"><p>&nbsp;<br />
+  <span style="color:#F19300;font-style:italic">Quand souhaitez-vous être contacté ?</span></p></th>
+</tr>
+<tr class="textcharte"><td>Jour</td><td>
+<input name='jour' type='radio' value='lundi' />Lundi<br />
+<input name='jour' type='radio' value='mardi' />Mardi<br />
+<input name='jour' type='radio' value='mercredi' />Mercredi<br />
+<input name='jour' type='radio' value='jeudi' />Jeudi<br />
+<input name='jour' type='radio' value='vendredi' />Vendredi<br />
+<input name='jour' type='radio' value='samedi' />Samedi<br />
+</td></tr>
+<tr class="textcharte"><td>Horaire</td>
+<td><input name='horaire' type='radio' value='matin' />En matinée <em>(de 9h à 12h)</em><br />
+<input name='horaire' type='radio' value='apres-midi' />Dans l'aprés-midi <em>(de 12h à 17h)</em><br />
+<input name='horaire' type='radio' value='soir' />En soirée <em>(de 17h à 20h)</em><br /></td></tr>
+<tr>
+  <th colspan="2" class="textbook"><p>&nbsp;<br />
+    <span style="color:#F19300;font-style:italic">Comment nous avez-vous connus ?</span></p></th>
+</tr>
+<tr class="textcharte"><td>&nbsp;</td><td>
+<input name='source' type='radio' value='internet' />Internet<br />
+<input name='source' type='radio' value='presse' />Presse<br /><input name='sujet' type='radio' value='salon' />Salon<br />
+<input name='source' type='radio' value='recommandation' />Recommandation<br />
+<input name='source' type='radio' value='autres' />Autres : <input type="text" name="autre_source" /></td></tr>
+<tr>
+  <th colspan="2" class="textbook"><p>&nbsp;</p></th>
+    </tr>
+<tr><td><input name='annuler' type='reset' value="Annuler" /></td><td>
+<input name="soumettre" type="button" value="Envoyer" onclick="valid_form()"/></td></tr>
+</table>
+</form>
+
+<?php
+}
+?>
+</td>
+    <td >&nbsp;</td>
+  </tr>
+  <tr>
+    <td width="170">&nbsp;</td>
+    <td colspan="3" ><div class="footer" align="center"><a class="menufooter" href="http://www.localhost:8000/">Accueil</a> &nbsp; | &nbsp; <a class="menufooter" href="http://www.localhost:8000/prestations/courtier-travaux-estimation-travaux-batiment-renovation.html">Prestations</a> &nbsp; | &nbsp; <a class="menufooter" href="http://www.localhost:8000/realisations/travaux-renovation-construction-maison-travaux-peinture-loiret-45.html">Réalisations</a> &nbsp; | &nbsp; <a class="menufooter" href="http://www.localhost:8000/charte-qualite/artisans-qualifies-devis-renovation-travaux-renovation.html">Charte Qualité</a> &nbsp; | &nbsp; <a class="menufooter" href="http://www.localhost:8000/partenaires/partenaires-informations-artisans-consult-courtier-travaux.html">Partenaires et Infos</a> &nbsp; | &nbsp; <a class="menufooter" href="http://www.localhost:8000/handibat.html">Handibat</a> &nbsp; | &nbsp; <a class="menufooter" href="http://www.localhost:8000/demande-renseignements-estimation-travaux-loiret-45.php">Contact</a> &nbsp; | &nbsp; <a class="menufooter" href="http://www.localhost:8000/mentionslegales.html">Mentions Légales</a></div></td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td width="170" align="right" valign="top"><div align="right"><img src="images/motifAC.gif" width="34" height="34" align="top" /></div></td>
+    <td colspan="3"><div align="center"><a href="http://www.nouveauchapitre.com" target="_blank" class="infolegalnb">Création nouveauchapitre.com, studio graphique.</a><a class="infolegal">©2016 tous droits réservés ARTISANS CONSULT.</a></div></td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+</body>
+</html>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-31584344-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
---
+++ slideshow/example_1.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+    // Set up our options for the slideshow...
+    var myOptions = {
+        noImages: 6,
+        path: "slideshow/slideshow_images/",  // Relative path with trailing slash.
+        links: { // Each image number must be listed here, unless no links are required at all, then links option can be ommitted.
+            1:"http://www.localhost:8000/realisations/travaux-renovation-construction-maison-travaux-peinture-loiret-45.html",
+            2:"http://www.localhost:8000/prestations/courtier-travaux-estimation-travaux-batiment-renovation.html",
+            3:"http://www.localhost:8000/charte-qualite/artisans-qualifies-devis-renovation-travaux-renovation.html",
+            4:"http://www.localhost:8000/partenaires/partenaires-informations-artisans-consult-courtier-travaux.html",
+            5:"http://www.localhost:8000/partenaires/partenaires-informations-artisans-consult-courtier-travaux.html",
+            6:"http://www.localhost:8000/partenaires/partenaires-informations-artisans-consult-courtier-travaux.html"
+        },
+        linksOpen:'newWindow',
+        timerInterval: 6500, // 6500 = 6.5 seconds
+	randomise: false // Start with random image?
+    };
+
+    // Woo! We have a jquery slideshow plugin!
+    $('#example_1_container').easySlides(myOptions);
+
+})
